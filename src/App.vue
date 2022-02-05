8ae17a4b2581312b5603be675e1498d6d755022a
LEEWONHOI <dldnjsghl92@gmail.com>
2022-02-05 13:21:32 +0900

    preject create

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <!-- Remove & use example component! -->
+  <HelloWorld :msg="message" @click="handler" />
+  <HelloWorld msg="Good" />
+  <div />
+</template>
+
+<script>
+import HelloWorld from "~/components/HelloWorld";
+
+export default {
+  components: {
+    HelloWorld
+  },
+  data() {
+    return {
+      message: "Hello Vue!"
+    };
+  },
+  methods: {
+    handler() {
+      console.log(this.message);
+      this.message += "!";
+    }
+  }
+};
+</script>
